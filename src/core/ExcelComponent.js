51cde987cd7e3d5b15a6d22f13549f226da461fa
Dmitry <arhidiman@mail.ru>
2023-11-29 21:53:36 +0700

    03_03 fix: exlint config

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -1,5 +1,5 @@
 import {DomListener} from "@core/DomListener";
-export class ExcelComponent extends DomListener  {
+export class ExcelComponent extends DomListener {
     toHTML() {
         return ""
     }
